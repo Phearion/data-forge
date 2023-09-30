c44a13c8cbae24a78458a8614b19827f7b0b1933
PCK <charles.kantane@outlook.fr>
2023-10-01 00:37:19 +0200

    Getting data for several subjects and topics. Problem with accents in the output represented in hexa

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import os
 import openai
 from dotenv import load_dotenv
-from prompt import prompt
+import prompt
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -16,6 +16,8 @@ class OpenAIGenerator:
 
     def __init__(self):
         # Set the API key
+        with open("DataForgeConfig.json", "r", encoding="utf-8") as file:
+            self.config = json.load(file)
         self.prompt = prompt
         self.response = None
 
@@ -31,23 +33,23 @@ class OpenAIGenerator:
                     "content": self.prompt,
                 },
             ],
-            temperature=0.3,
+            temperature=0.5,
             max_tokens=2000,
         )
 
         # say if the response is ready
         print("response ready")
 
-    def generate_csv(self):
+    def generate_csv(self, subject):
         """
         Generate a csv file from the response.
         """
         response = self.response["choices"][0]["message"]["content"]
         try:
             response_to_json = json.loads(response)
-            with open("llama.csv", "a", encoding="utf-8") as file:
+            with open(f"llama_{subject}_dataset.csv", "a", encoding="utf-8") as file:
                 # if file is not empty don't write the header
-                if os.stat("llama.csv").st_size == 0:
+                if os.stat(f"llama_{subject}_dataset.csv").st_size == 0:
                     file.write("instruction,input,output\n")
 
                 for data in response_to_json:
@@ -60,15 +62,20 @@ class OpenAIGenerator:
                         )
                     except KeyError:
                         pass
-        except json.decoder.JSONDecodeError:
+        except json.decoder.JSONDecodeError as e:
             print("JSONDecodeError")
+            print(e)
 
 
 if __name__ == "__main__":
     # create a class
     generator = OpenAIGenerator()
     # get the model
-    for i in range(3):
-        print(f"iteration {i}")
-        generator.model()
-        generator.generate_csv()
+    for key in generator.config["themes_dict"].keys():
+        print(f"subject: {key}")
+        generator.prompt = prompt.get_prompt(subject=key)
+
+        for i in range(generator.config["nb_iterations"]):
+            print(f"iteration {i + 1}")
+            generator.model()
+            generator.generate_csv(subject=key)
--- prompt.py
+++ prompt.py
@@ -1,27 +1,59 @@
-prompt = ''' 
-Act as a data parser with those 3 elements: "instructions, input and output\n" 
-instruction will always be the same: \"You are a data parser aim to help students finding ressources, answer with a json format as best as possible."\n 
-input will be something that a student need for their study, example: "I need help with mathematics"\n 
-output should be an answer to the input as a JSON format.\n" 
-\nGive me a JSON format that is an array of hundreds of instruction,input,output this way:" 
-[
-         {
-            instruction: "You are a data parser aim to help students finding ressources, answer with a json format as best as possible.",
-            input: "I need help with mathematics",
-            output: {
-                "topic": "maths",
-                "subject": "general"
-            }
-        },
-        {
-            instruction: "You are a data parser aim to help students finding ressources, answer with a json format as best as possible.",
-            input: "I need to understand better matrices  ",
-            output: {
-                "topic": "maths",
-                "subject": "matrices"
-            }
-        }
-]
+import json
 
-IMPORTANT: Don't give same example twice, it should be unique.
-'''
+with open("DataForgeConfig.json", "r", encoding="utf-8") as file:
+    config = json.load(file)
+
+
+def get_prompt(subject):
+    return '''
+    Act as a data parser with those 3 elements: "instructions, input and output\n"
+    Instruction will always be the same: \"You are a data parser aim to help students finding resources, answer with a
+    json format as best as possible."\n
+    Input will be something that a student need for their study. It could be formulated as a sentence or a question.
+    In any case, it should looks like a request that a student could ask, for example : "I need help with
+    mathematics""\n
+    You can adopt any structure you like to build the request."\n
+    Student requests MUST ONLY be about the topics and the subjects that are present in the dictionary: %s (the key is
+    the subject and the value is the topics list)""
+    The subject you will be tackling now is: %s.
+    Here are some examples of structure for sentence starting that could be used:"\n
+    Could you please help me to,
+    I'm struggling to,
+    I'm not sure how to,
+    Can you help me to improve,
+    I'm stuck on,
+    I'm having trouble with,
+    Do you have any tips for,
+    Could you give me some advice on"\n
+    Gather some concepts from the subjects and topic lists and build requests according them. The requests MUST only be
+    about them. It's very important !! "\n
+    If the topic name is in an output (for example: "I need help in algebra"), try your best to add a concept in that
+    topic or be specific in the request."\n
+    You MUST NOT create requests that are too general.\n
+    output should be an answer to the input as a JSON format.\n"
+    \nGive me a JSON format that is an array of EXACTLY %s lines (instructions,inputs,outputs) this way:"
+    [
+             {
+                instruction: "You are a data parser aim to help students finding resources, answer with a json format
+                as best as possible.",
+                input: "I need help with mathematics",
+                output: {
+                    "topic": ...,
+                    "subject": ...
+                }
+            },
+            {
+                instruction: "You are a data parser aim to help students finding resources, answer with a json format as
+                best as possible.",
+                input: "I need to understand better matrices  ",
+                output: {
+                    "topic": ...,
+                    "subject": ...
+                }
+            }
+    ]
+    \n
+    The subject and the topic MUST be the same as the written ones in the dictionary %s.
+
+    IMPORTANT: DO NOT give same example twice, it should be unique. Only the input and instruction should be in french.
+    ''' % (config["themes_dict"], subject, config["nb_observations"], config["themes_dict"])
