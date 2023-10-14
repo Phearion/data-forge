632f17e2e2534547aebf349557ee73d610fe5ffb
Pritam Kantane <78143876+PyyLog@users.noreply.github.com>
2023-10-14 17:54:56 +0200

    feat: duplicates removal algorithm (#5)

---
+++ duplicates_verification.py
@@ -0,0 +1,33 @@
+import csv
+import pandas as pd
+
+
+class DuplicatesVerification:
+    """
+    Duplicate verification class to verify if there are duplicates in the CSV files.
+    """
+    def __init__(self, file):
+        self.file = file
+        self.base_nb_observations = 0
+        self.nb_duplicates = 0  # nb of duplicates that were removed
+
+    def verify_duplicates(self):
+        """
+        Verify if there are duplicates in the CSV files.
+        """
+        df = pd.read_csv(self.file, encoding="utf-8")
+        self.base_nb_observations = df.shape[0]
+
+        # check for duplicates in the input column
+        df_duplicates = df[df.duplicated(subset=["input"], keep=False)]
+
+        if df_duplicates.empty:
+            print(f"{self.file}: no duplicates")
+        else:
+            # remove duplicates
+            df.drop_duplicates(subset=["input"], keep="first", inplace=True)
+
+            # save the dataframe to a new csv file
+            df.to_csv(self.file, index=False, encoding="utf-8", quoting=csv.QUOTE_NONE)
+            self.nb_duplicates = self.base_nb_observations - df.shape[0]
+            print(f"{self.file}: {self.nb_duplicates} duplicates removed")
